f9e510cf2c98695fb1e529343ef4d1680e0e79bf
rodrigomartinez <el.friki666@gmail.com>
2012-03-15 08:50:44 +0100

    added interface .png and cube sliders

--- src/Logic/ModuleColorPalette/ColorPalette.cpp
+++ src/Logic/ModuleColorPalette/ColorPalette.cpp
@@ -3,6 +3,9 @@
 #include <QtGui/QImage>
 #include <QtGui/QColor>
 
+#include <QtGui/QImage>
+#include <QtGui/QColor>
+
 // Here goes member definition
 
 
@@ -34,6 +37,8 @@ void ColorPalette::ProcessSquares (const Square **recognizedSquares, size_t size
 		}
 
 	}
+//careful here: control repeated cubes, control not present cubes (defaulting?)
+
 
 	CvPoint bluepoint	= blue->GetCenterCoordinates();
 	CvPoint redpoint	= red->GetCenterCoordinates();
@@ -41,11 +46,62 @@ void ColorPalette::ProcessSquares (const Square **recognizedSquares, size_t size
 
 	printf("xs of rgb: %d %d %d\n", redpoint.x, greenpoint.x, bluepoint.x);
 
-	float redAmount		= (float)256 * ((float)redpoint.x/(float)100);
-	float greenAmount	= (float)256 * ((float)greenpoint.x/(float)100);
-	float blueAmount	= (float)256 * ((float)bluepoint.x/(float)100);
+	float redAmount		= 256 * ((float)redpoint.x/100);
+	float greenAmount	= 256 * ((float)greenpoint.x/100);
+	float blueAmount	= 256 * ((float)bluepoint.x/100);
 
 	printf("Result: %f - %f - %f\n", redAmount, greenAmount, blueAmount);
+
+
+	//new code from here
+
+
+
+	QImage *image = new QImage(QSize(640, 640), QImage::Format_RGB16);
+	QColor *color = new QColor(redAmount,greenAmount,blueAmount, 255);
+
+
+	image->load("interface.png");
+
+	//Draws a cube in the red slider
+	for (int i=1; i<50; i++)
+	{
+		for (int j=1; j<50; j++)
+		{
+			image->setPixel(i+(redAmount*(640/255)),j+135,qRgb(0,0,0));
+		}
+	}
+	//Draws a cube in the green slider
+	for (int i=1; i<50; i++)
+	{
+		for (int j=1; j<50; j++)
+		{
+				image->setPixel(i+(greenAmount*(640/255)),j+245,qRgb(0,0,0));
+		}
+	}
+
+	//Draws a cube in the blue slider
+	for (int i=1; i<50; i++)
+	{
+		for (int j=1; j<50; j++)
+		{
+			image->setPixel(i+(blueAmount*(640/255)),j+357,qRgb(0,0,0));
+		}
+	}
+	//Draws the chosen color
+	for (int i=1; i<120; i++)
+	{	for (int j=1; j<70; j++)
+		{
+			image->setPixel((i+250),j+500,color->rgb());
+		}
+
+	}
+
+	//image->fill(color->rgb());
+
+
+	image->save("secondtest.png");
+
 
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
 	ColorPalette *palette = new ColorPalette();
 
-	CvPoint p1 = cvPoint(100, 4);
+	CvPoint p1 = cvPoint(0, 4);
 	Square *sq1 = new Square(1, p1, 100, 6);
 
 	CvPoint p2 = cvPoint(50, 4);
@@ -23,10 +23,52 @@ int main(int argc, char *argv[])
 
 	palette->ProcessSquares((const Square **)squares, 3);
 
+	int color1=0;
+	int color2=128;
+	int color3=255;
 
-	QImage *image = new QImage(QSize(640, 480), QImage::Format_RGB16);
-	QColor *color = new QColor(255,128,255, 255);
-	image->fill(color->rgb());
+	QImage *image = new QImage(QSize(640, 640), QImage::Format_RGB16);
+	QColor *color = new QColor(color1,color2,color3, 255);
+
+
+	image->load("interface.png");
+
+	//Draws a cube in the red slider
+	for (int i=1; i<50; i++)
+	{
+		for (int j=1; j<50; j++)
+		{
+			image->setPixel(i+(color1*(640/255)),j+135,qRgb(0,0,0));
+		}
+	}
+	//Draws a cube in the green slider
+	for (int i=1; i<50; i++)
+	{
+		for (int j=1; j<50; j++)
+		{
+				image->setPixel(i+(color2*(640/255)),j+245,qRgb(0,0,0));
+		}
+	}
+
+	//Draws a cube in the blue slider
+	for (int i=1; i<50; i++)
+	{
+		for (int j=1; j<50; j++)
+		{
+			image->setPixel(i+(color3*(640/255)),j+357,qRgb(0,0,0));
+		}
+	}
+	//Draws the chosen color
+	for (int i=1; i<120; i++)
+	{	for (int j=1; j<70; j++)
+		{
+			image->setPixel((i+250),j+500,color->rgb());
+		}
+
+	}
+
+	//image->fill(color->rgb());
+
 
 	image->save("test.png");
 
