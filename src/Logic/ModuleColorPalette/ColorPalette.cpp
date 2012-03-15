ada792b6626e8d64e07ecc76033874127b15ae93
Vojto Rinik <vojto@rinik.net>
2012-03-15 02:42:07 -0700

    Implementing the slider

--- src/Logic/ModuleColorPalette/ColorPalette.cpp
+++ src/Logic/ModuleColorPalette/ColorPalette.cpp
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <QtGui/QImage>
 #include <QtGui/QColor>
-
+#include <QtGui/QPainter>
 #include <QtGui/QImage>
 #include <QtGui/QColor>
 
@@ -46,62 +46,35 @@ void ColorPalette::ProcessSquares (const Square **recognizedSquares, size_t size
 
 	printf("xs of rgb: %d %d %d\n", redpoint.x, greenpoint.x, bluepoint.x);
 
-	float redAmount		= 256 * ((float)redpoint.x/100);
-	float greenAmount	= 256 * ((float)greenpoint.x/100);
-	float blueAmount	= 256 * ((float)bluepoint.x/100);
+	int redAmount		= 255 * ((float)redpoint.x/100);
+	int greenAmount	= 255 * ((float)greenpoint.x/100);
+	int blueAmount	= 255 * ((float)bluepoint.x/100);
 
-	printf("Result: %f - %f - %f\n", redAmount, greenAmount, blueAmount);
-
-
-	//new code from here
-
-
-
+	// Prepare the UI images
 	QImage *image = new QImage(QSize(640, 640), QImage::Format_RGB16);
-	QColor *color = new QColor(redAmount,greenAmount,blueAmount, 255);
-
-
 	image->load("interface.png");
+	QImage *ball = new QImage(QSize(52, 54), QImage::Format_RGB16);
+	ball->load("ball.png");
 
-	//Draws a cube in the red slider
-	for (int i=1; i<50; i++)
-	{
-		for (int j=1; j<50; j++)
-		{
-			image->setPixel(i+(redAmount*(640/255)),j+135,qRgb(0,0,0));
-		}
-	}
-	//Draws a cube in the green slider
-	for (int i=1; i<50; i++)
-	{
-		for (int j=1; j<50; j++)
-		{
-				image->setPixel(i+(greenAmount*(640/255)),j+245,qRgb(0,0,0));
-		}
-	}
+	// Compose the color
+	QColor *color = new QColor(redAmount, greenAmount, blueAmount, 255);
+	printf("Composing the color: [%d, %d, %d] => %lu\n", redAmount, greenAmount, blueAmount, color->rgb());
 
-	//Draws a cube in the blue slider
-	for (int i=1; i<50; i++)
-	{
-		for (int j=1; j<50; j++)
-		{
-			image->setPixel(i+(blueAmount*(640/255)),j+357,qRgb(0,0,0));
-		}
-	}
-	//Draws the chosen color
+	// Prepare the painter
+	QPainter *painter = new QPainter(image);
+
+	// Draw ball for each color
+	painter->drawImage(redAmount, 135, *ball);
+	painter->drawImage(blueAmount, 245, *ball);
+	painter->drawImage(redAmount, 355, *ball);
+
+	// Draw the chosen color
 	for (int i=1; i<120; i++)
-	{	for (int j=1; j<70; j++)
-		{
+		for (int j=1; j<70; j++)
 			image->setPixel((i+250),j+500,color->rgb());
-		}
 
-	}
-
-	//image->fill(color->rgb());
-
-
-	image->save("secondtest.png");
-
+	// Save the image
+	image->save("test.png");
 
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <QtGui/QImage>
 #include <QtGui/QColor>
+#include <QtGui/QFont>
+#include <QtGui/QPainter>
+#include <QtGui/QApplication>
 
 #include "Recognition/Square.h"
 #include "Logic/ModuleColorPalette/ColorPalette.h"
@@ -8,6 +11,8 @@
 
 int main(int argc, char *argv[])
 {
+	QApplication *app = new QApplication(argc, argv);
+
 	ColorPalette *palette = new ColorPalette();
 
 	CvPoint p1 = cvPoint(0, 4);
@@ -23,56 +28,5 @@ int main(int argc, char *argv[])
 
 	palette->ProcessSquares((const Square **)squares, 3);
 
-	int color1=0;
-	int color2=128;
-	int color3=255;
-
-	QImage *image = new QImage(QSize(640, 640), QImage::Format_RGB16);
-	QColor *color = new QColor(color1,color2,color3, 255);
-
-
-	image->load("interface.png");
-
-	//Draws a cube in the red slider
-	for (int i=1; i<50; i++)
-	{
-		for (int j=1; j<50; j++)
-		{
-			image->setPixel(i+(color1*(640/255)),j+135,qRgb(0,0,0));
-		}
-	}
-	//Draws a cube in the green slider
-	for (int i=1; i<50; i++)
-	{
-		for (int j=1; j<50; j++)
-		{
-				image->setPixel(i+(color2*(640/255)),j+245,qRgb(0,0,0));
-		}
-	}
-
-	//Draws a cube in the blue slider
-	for (int i=1; i<50; i++)
-	{
-		for (int j=1; j<50; j++)
-		{
-			image->setPixel(i+(color3*(640/255)),j+357,qRgb(0,0,0));
-		}
-	}
-	//Draws the chosen color
-	for (int i=1; i<120; i++)
-	{	for (int j=1; j<70; j++)
-		{
-			image->setPixel((i+250),j+500,color->rgb());
-		}
-
-	}
-
-	//image->fill(color->rgb());
-
-
-	image->save("test.png");
-
-
-
-    return 0;
+	return 0;
 }
